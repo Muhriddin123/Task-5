a4d67dd456aed0d2a05d283be0a31d93a625e73d
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-10-28 01:19:10 +0600

    Task

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,16 @@
+package web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+    private String mark;
+    private String model;
+    private int year;
+
+
+}
